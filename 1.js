2dc97ee49fc17e7b386abe103d8ead9bb76e6452
Jessica <j.jecristina@gmail.com>
2025-05-08 18:34:59 -0300

    atividade 1

--- 1.js
+++ 1.js
@@ -1,6 +1,25 @@
-console.log("hello");
-console.log("hello");
-console.log("hello");
-console.log("hello");
-console.log("hello");
-console.log("hello");
+// Escreva um programa para calcular a redução do tempo de vida de um fumante.
+// Pergunte a quantidade de cigarros fumados por dias e quantos anos ele já fumou.
+// Considere que um fumante perde 10 min de vida a cada cigarro. Calcule quantos dias de
+// vida um fumante perderá e exiba o total em dias.
+
+const prompt = require("prompt-sync")()
+
+let cigarrosPorDia = Number(prompt("Quantos cigarros você fuma por dia? "));
+let anosFumando = Number(prompt("Quantos anos você fuma? "));
+
+
+const diasNoAno = 365
+const totalCigarros = cigarrosPorDia * anosFumando * diasNoAno;
+
+
+const minutosPorCigarro = 10;
+const totalMinutosPerdidos = totalCigarros * minutosPorCigarro;
+
+
+const minutosPorHora = 60;
+const horasPorDia = 24;
+const totalDiasPerdidos = totalMinutosPerdidos / (minutosPorHora * horasPorDia);
+
+
+console.log(`Você perdeu aproximadamente ${totalDiasPerdidos.toFixed(2)} dias de vida.`);
