92b34390658a58bf5cb7fca6a7dfc37561e754f3
Jessica <j.jecristina@gmail.com>
2025-05-09 17:26:06 -0300

    atividade 13

---
+++ 13.js
@@ -0,0 +1,11 @@
+/* Crie um programa que preencha automaticamente (usando lógica, não apenas
+atribuindo diretamente) um vetor numérico com 15 posições com os primeiros elementos
+da sequência de Fibonacci. */
+
+let fibonacci = [1, 1];
+
+for (let i = 2; i < 15; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+
+console.log(fibonacci);
